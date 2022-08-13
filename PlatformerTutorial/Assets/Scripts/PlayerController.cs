fbd2e3c859802959d93520c123b9bc43ed516a40
Keane Osteen <osteenkeane@gmail.com>
2022-08-13 16:57:54 -0500

    Jumping added to player controller

--- PlatformerTutorial/Assets/Scripts/PlayerController.cs
+++ PlatformerTutorial/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,9 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public Vector2 movement;
     // // x y
+    public float jumpSpeed;
     public Rigidbody2D rb;
+    public bool isGrounded = true;
 
     // Start is called before the first frame update
     void Start()
@@ -23,5 +25,16 @@ public class PlayerController : MonoBehaviour
 
         //rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
         rb.AddForce(movement * speed + rb.position * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            rb.AddForce(new Vector2(0, jumpSpeed) + rb.position * Time.deltaTime);
+            isGrounded = false;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        isGrounded = true;
     }
 }
